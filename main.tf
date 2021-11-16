1f60776b75bcc5fbd110523b437c772c409abd64
Paul Collas <paulcollas446@gmail.com>
2021-11-16 11:29:06 +0100

    NEW : end of exercise 2 - create database

--- main.tf
+++ main.tf
@@ -8,8 +8,17 @@ terraform {
 }
 
 provider "scaleway" {
-  zone            = "fr-par-1"
-  region          = "fr-par"
+  access_key = "SCW5X569PRAJ6Q3X1EKT"
+  secret_key = "8b005a8a-635e-4372-9a86-1967b204a73c"
+  project_id = "785298d6-80d8-442d-9dda-1831e3bb5020"
 }
 
-# resource "scaleway_instance_ip" "public_ip" {}
+resource "scaleway_rdb_instance" "main" {
+  name           = "devops-rdb"
+  node_type      = "DB-DEV-S"
+  engine         = "PostgreSQL-12"
+  is_ha_cluster  = true
+  # disable_backup = true
+  # user_name      = "user"
+  # password       = "password"
+}
